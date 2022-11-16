7e4c0f8d10cbedb54ce1a965e9e7eaf9c8709dce
manucomh <pmaucayllehuaman@gmail.com>
2022-11-16 11:32:31 -0500

    regions country avance

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ window.addEventListener("hashchange", () => {
 
 
 const url = 'https://restcountries.com/v3.1/all';
-var countries_container = document.getElementById('container_items_contries');
+
+const countries_container = document.getElementById('container_items_contries');
+const buttons_regions = document.querySelectorAll('.ul_regions > li a');
+const button_search = document.querySelector('form .column_left .line .button_search');
+
 
 async function fetchData(url){
     const response = await fetch(url);
@@ -37,10 +41,50 @@ function printView(item){
     
     return `
             <div class="item">
-                <h1 class="name_country">${item.name.common}</h1>    
+                <img src="${item.flags.png}">
+                <h2 class="name_country">${item.name.common}</h2> 
+                <h3 class="region">${item.continents}</h3>
             </div>
         `;              ////interpolando valor
 }
+
+///https://www.youtube.com/watch?v=FqcRTO3rYWw&t=737s  link del ayudnatia 09/11
+const  fetchRegion = async (e) => {
+    const response = await fetch(`https://restcountries.com/v3.1/region/${e}`);
+    const data = await response.json();
+    return data;
+};
+
+const fetchByName = async (e) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${e}`);
+    const data = await response.json();
+    return data;
+}
+
+const makeSearch = async (region) => {
+    const data = await fetchRegion(region);
+    const template = data.map((element) => printView(element))
+    countries_container.innerHTML=template.join('');
+}
+const makeSearchCountry = async (country) => {
+    const data = await fetchByName(country);
+    const template = data.map((element) => printView(element))
+    countries_container.innerHTML=template.join('');
+}
+
+buttons_regions.forEach((element)=>{
+    element.addEventListener("click",(event) => makeSearch(event.target.textContent));
+});
+
+
+button_search.addEventListener('click',(e) =>{
+    const textSearch = document.querySelector('form .row_container_widgets .column_left #input_search');
+    if(textSearch.value != ""){
+        makeSearchCountry(textSearch.value);
+    }else{
+        alert('Ingrese Country');
+    }
+});
 
 
 window.addEventListener('load',function(){
@@ -48,48 +92,31 @@ window.addEventListener('load',function(){
 });
 
 
+/** prototype eleminar espacio vacios al inicio y al final */
 
-/*
-
-function getCountries(){
-    const res = fetch(url);
-    // regresa una promesa
-    return res;
-}
-
-
-
-getCountries().then(item => item.json()).then(item_country => {
-    item_country.map(post => {
-        const i = document.createElement('a')
-        // console.log(i);
-        console.log(post.name.official);
-        i.innerHTML = post.name.common+'<br>';
-        countries_container.appendChild(i);
-    })
-});
-
-function data_person(name, lastname, edad, birthday){
-    return {name, lastname, edad, birthday};
-}
-
-var juan = data_person('juan', 'Huaman Huaman', 27, new Date(Date.now()));
-console.log(juan);
+String.prototype.trimLeft = String.prototype.trimLeft || function () {
+    return this.replace(/^\s+/, "");
+};
+ 
+String.prototype.trimRight = String.prototype.trimRight || function () {
+    return this.replace(/\s+$/, "");
+};
 
 
-// dropdown button
+/**
+ * dropdown regions button
+ */
+
 var button_dropdown_toggle = document.getElementById('button_dropdown_toggle');
 button_dropdown_toggle.addEventListener('click',(element)=>{
-    regions = document.getElementById('ul_regions');
-    icon = document.getElementById('icon_filer_caret');
+    var regions = document.getElementById('ul_regions');
+    var icon = document.getElementById('icon_filer_caret');
     if(regions.style.display == 'none' || regions.style.display == ''){
         regions.style.display = 'block';
-        // regions.style.transition = 'height 2s';
+        regions.style.transition = "transition: all 2s ease-in-out"
         icon.className = 'fas fa-caret-right';
     }else if(regions.style.display == 'block'){
         regions.style.display = 'none';
         icon.className = 'fas fa-caret-down';
     }
 });
-
-*/
